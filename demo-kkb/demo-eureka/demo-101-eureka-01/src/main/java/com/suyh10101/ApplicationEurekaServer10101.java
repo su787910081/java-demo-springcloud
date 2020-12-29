9794b9af0082239707f12cd54dc91aa144ddfe14
苏云弘 <su_787910081@sina.com>
2020-12-29 21:52:41 +0800

    kkb eureka demo

---
+++ demo-kkb/demo-eureka/demo-101-eureka-01/src/main/java/com/suyh10101/ApplicationEurekaServer10101.java
@@ -0,0 +1,11 @@
+package com.suyh10101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEurekaServer10101 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationEurekaServer10101.class, args);
+    }
+}
