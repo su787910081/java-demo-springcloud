5564342966b3bbf1eefb61e61b80cf6a6e77e1c2
苏云弘 <su_787910081@sina.com>
2021-01-01 01:57:16 +0800

    spring-cloud-demo

--- demo-kkb/demo-02-openfeign/demo-221-consumer-feign/src/main/java/com/suyh1221/service/DepartService.java
+++ demo-kkb/demo-02-openfeign/demo-221-consumer-feign/src/main/java/com/suyh1221/service/DepartService.java
@@ -1,6 +1,7 @@
 package com.suyh1221.service;
 
 import com.suyh1221.bean.Depart;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
