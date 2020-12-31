c6694f0644781bdd6541230e3878e3a975d0bfae
苏云弘 <su_787910081@sina.com>
2021-01-01 02:03:11 +0800

    spring-cloud-demo

--- demo-kkb/demo-02-openfeign/demo-221-consumer-feign/src/main/java/com/suyh1221/ApplicationFeignConsumer1221.java
+++ demo-kkb/demo-02-openfeign/demo-221-consumer-feign/src/main/java/com/suyh1221/ApplicationFeignConsumer1221.java
@@ -2,7 +2,9 @@ package com.suyh1221;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients  // 开启Feign客户端
 @SpringBootApplication
 public class ApplicationFeignConsumer1221 {
     public static void main(String[] args) {
