5714c4dc76a826741982e95d7653d2b2f7f92301
苏云弘 <su_787910081@sina.com>
2021-01-05 23:35:54 +0800

    spring-cloud-demo

---
+++ demo-kkb/demo-03-hystrix/demo-311-provider-hystrix/src/main/java/suyh311/service/DepartService.java
@@ -0,0 +1,13 @@
+package suyh311.service;
+
+import com.abc.bean.Depart;
+
+import java.util.List;
+
+public interface DepartService {
+    boolean saveDepart(Depart depart);
+    boolean removeDepartById(Integer id);
+    boolean modifyDepart(Depart depart);
+    Depart getDepartById(int id);
+    List<Depart> listAllDeparts();
+}
