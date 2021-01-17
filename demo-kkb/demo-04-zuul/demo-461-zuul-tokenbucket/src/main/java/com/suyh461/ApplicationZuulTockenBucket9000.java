7ca080aa27d6f80c7110d19e0dcae2eba7108b69
苏云弘 <su_787910081@sina.com>
2021-01-17 16:35:33 +0800

    commit

---
+++ demo-kkb/demo-04-zuul/demo-461-zuul-tokenbucket/src/main/java/com/suyh461/ApplicationZuulTockenBucket9000.java
@@ -0,0 +1,16 @@
+package com.suyh461;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApplicationZuulTockenBucket9000 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationZuulTockenBucket9000.class, args);
+    }
+
+
+}
