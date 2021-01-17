fab2f5dfd6c9c0c85dae15f2381dc8467e5071b6
苏云弘 <su_787910081@sina.com>
2021-01-17 18:20:48 +0800

    commit

---
+++ demo-kkb/demo-04-zuul/demo-421-consumer-gray/src/main/java/com/suyh421/ApplicationConsumer8180.java
@@ -0,0 +1,13 @@
+package com.suyh421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class ApplicationConsumer8180 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConsumer8180.class, args);
+    }
+
+}
