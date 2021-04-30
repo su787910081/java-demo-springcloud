13618b028367c183f34aa8f35a24b9821c601c87
苏云弘 <su_787910081@sina.com>
2021-04-30 22:40:39 +0800

    commit

---
+++ demo-kkb/demo-05-gateway/demo-521-gateway-api/src/main/java/com/suyh521/GatewayApiApplication521.java
@@ -0,0 +1,12 @@
+package com.suyh521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication521 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApiApplication521.class, args);
+    }
+
+}
---
+++ demo-kkb/demo-05-gateway/demo-521-gateway-api/src/main/java/com/suyh521/config/GatewayRouteConfiguration.java
@@ -0,0 +1,25 @@
+package com.suyh521.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class GatewayRouteConfiguration {
+
+    //配置路由规则
+    @Bean
+    public RouteLocator someRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(predicateSpec ->
+                        predicateSpec.path("/")
+                                .uri("https://www.baidu.com")
+                                .id("baidu_route"))
+                // TODO: suyh - 这后面的163 没有生效呀。
+                .route(predicateSpec ->
+                        predicateSpec.path("/**/163")
+                                .uri("https://www.163.com/")
+                                .id("163_route")).build();//函数式编程思想
+    }
+}
