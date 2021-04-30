072fbcaf59f219fc5b9b80aeec304306d4118c0a
苏云弘 <su_787910081@sina.com>
2021-04-30 22:27:02 +0800

    commit

---
+++ demo-kkb/demo-05-gateway/demo-511-gateway-config/src/main/java/com/suyh511/GatewayConfigApplication511.java
@@ -0,0 +1,11 @@
+package com.suyh511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayConfigApplication511 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayConfigApplication511.class, args);
+    }
+}
