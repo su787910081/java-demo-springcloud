6b35a89bde3c26dd31b1802e31d951fd27692000
suyh <su_787910081@sina.com>
2021-05-02 20:21:33 +0800

    commit

--- demo-kkb/demo-05-gateway/demo-501-provider/src/main/java/com/abc/repository/DepartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abc.repository;
-
-import com.abc.bean.Depart;
-import org.springframework.data.jpa.repository.JpaRepository;
-//持久层：在中国Mybatis非常流行灵活，但是Hibernate纯ORM
-public interface DepartRepository extends JpaRepository<Depart, Integer> {
-}
